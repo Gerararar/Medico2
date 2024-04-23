673c48c3610c784b66edf026e388e22f763ae22e
ALEJANDRO GERARDO SALAZAR TOVAR <ag.salazartovar@ugto.mx>
2024-04-23 14:16:53 -0600

    Agregar archivos de proyecto.

---
+++ Medico/Services/CitasService.cs
@@ -0,0 +1,36 @@
+﻿using Medico.Data;
+using Medico.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Conventions;
+
+namespace Medico.Services
+{
+    public class CitasService (ApplicationDbContext context) : ICitas
+    {
+        public async Task<List<Cita>> ObtenerCitasHoy()
+        {
+            List<Cita> citas = new List<Cita>();
+            
+            citas = await context.Citas.Include(p => p.Paciente)
+                .OrderByDescending(f => f.Fecha).ToListAsync();
+            
+            return citas;
+        }
+
+        public async Task<List<Paciente>> Pacientes()
+        {
+            List<Paciente> pacientes = new List<Paciente>();
+
+            pacientes = await context.Pacientes.ToListAsync();
+
+            return pacientes;
+        }
+
+        public async Task<Cita> AgendarCita(Cita cita)
+        {
+            var respuesta = context.Citas.Add(cita);
+            await context.SaveChangesAsync();
+            return cita;
+        }
+    }
+}
---
+++ Medico/Services/ICitas.cs
@@ -0,0 +1,11 @@
+﻿using Medico.Models;
+
+namespace Medico.Services
+{
+    public interface ICitas
+    {
+        Task<List<Cita>> ObtenerCitasHoy();
+        Task<List<Paciente>> Pacientes();
+        Task<Cita> AgendarCita(Cita cita);
+    }
+}
